88433c1784fd5ed60fbba0a98f2737c799e16bea
MrShifu01 <stander.christian.com>
2023-07-24 07:53:30 +0200

    deploy test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:8000"
+// axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = "https://matumicorp.onrender.com"
 
 function App() {
   return (
